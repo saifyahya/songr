e6f3530c8f7286f67c94b0e0f45bb872a12bb58a
saifyahya <saifbarakat991@gmail.com>
2023-09-12 06:25:42 +0000

    lab12

--- src/main/java/com/saifyahya/songr/controller/AlbumController.java
+++ src/main/java/com/saifyahya/songr/controller/AlbumController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
-
+import java.util.Optional;
 
 
 @Controller
@@ -52,6 +52,42 @@ public class AlbumController {
         else
             return "albums.html";
     }
+    @GetMapping("/update-album")
+    public String updateAlbumForm(){
+        return "update-album.html";
+    }
+    @PostMapping("/update-album")
+    public RedirectView saveUpdatedAlbum(Long id,String title, String artist,  String songCount,  String lengthInSecond, String imageUrl) {
+            try {
+                Optional<Album> albumFromRepo = albumRepository.findById(id);  // optional type tha holds data or null from find byId method
+                if (albumFromRepo.isPresent()) {  // if the album exists in DB
+                    Album albumToUpdate = albumFromRepo.get();
+                Integer integerSongCount = Integer.parseInt(songCount);
+
+                Integer integerLength = Integer.parseInt(lengthInSecond);
+
+                if (!title.equals(""))
+                    albumToUpdate.setTitle(title);
+                if (!artist.equals(""))
+                    albumToUpdate.setArtist(artist);
+                if (integerSongCount!=null)
+                    albumToUpdate.setSongCount(integerSongCount);
+                if (integerLength!=null)
+                    albumToUpdate.setLengthInSecond(integerLength);
+                if (!imageUrl.equals(""))
+                    albumToUpdate.setImageUrl(imageUrl);
+                albumRepository.save(albumToUpdate);   //saving the updated album to database
+            } else{
+                return new RedirectView("/update-album"); //  this will trigger the getMapping for the given path
+
+        }
+    }catch(NumberFormatException e){
+                System.out.println("Error in parsing updated album: "+e);;
+                return new RedirectView("/albums");
+            }
+        return new RedirectView("/albums");   // if updated done successfully
+    }
+
     @DeleteMapping("/delete-album/{albumId}")
     public RedirectView deleteAlbum(@PathVariable Long albumId) {
         albumRepository.deleteById(albumId);
